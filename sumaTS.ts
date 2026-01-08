3e747a8f716b62e2a6f58f221377ca9146024bd8
JairDelaTorre <franklin.delatorre035@ist17dejulio.edu.ec>
2026-01-08 14:35:55 -0500

    Utilizar foreach en una lista para multiplicar los numeros

--- sumaTS.ts
+++ sumaTS.ts
@@ -1,9 +1,39 @@
-function sumar() {
-    const num1 = parseFloat((document.getElementById("num1") as HTMLInputElement).value;
-    const num2 = parseFloat((document.getElementById("num2") as HTMLInputElement).value;
-    if (num1>=0 && num2>=0){
-        let suma = num1 + num2;
-            document.getElementById("resultado").innerText = "la suma es: " + suma;
+//PRACTICA GENERAL DE LA SINTAXIS
+
+/*let num1:string|number|number[]=[1,2,4];
+//let lista[]:string|boolean|number|string[]=['hola',7,'ts',8,'ast',9];
+let num2:number=10;
+num1.forEach(e => {
+    let suma = e + num2;
+    console.log(" numero 1: " + e + " numero 2: " + num1 + " = " + num2 ); 
+});
+*/
+
+let lista: number[] = [1,2,3,4,5,6,7,8,9,10];
+let multiplicacion = 1;
+let listaPrimos: number[] = [];
+
+function esPrimo(numero: number): boolean {
+  if (numero <= 1) return false;
+
+  for (let i = 2; i < numero; i++) {
+    if (numero % i == 0) {
+      return false;
     }
-    
+  }
+  return true;
 }
+
+lista.forEach(e => { 
+  multiplicacion = multiplicacion * e;
+
+  if (esPrimo(e)) {
+    console.log(e + " es primo");
+    listaPrimos.push(e);
+  } else {
+    console.log(e + " NO es primo");
+  }
+});
+
+console.log("La multiplicación es: " + multiplicacion);
+console.log("Sublista de primos:", listaPrimos);
